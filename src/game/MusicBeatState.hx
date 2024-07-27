2b9c7e139d57dae9fb3b8827aa5cbc3a434d7bf4
x02mateus <soares.mateus201122@gmail.com>
2024-07-27 11:15:16 -0300

    Conversão do MusicBeat 😎

---
+++ src/game/MusicBeatState.hx
@@ -0,0 +1,160 @@
+package game;
+
+import flixel.addons.transition.FlxTransitionableState;
+
+class MusicBeatState extends flixel.addons.ui.FlxUIState
+{
+	private var lastBeat:Float = 0;
+	private var lastStep:Float = 0;
+
+	private var curStep:Int = 0;
+	private var curBeat:Int = 0;
+
+	// public var controls(get, never):Input.Controls;
+	public static var camBeat:FlxCamera;
+
+	/*inline function get_controls():Input.Controls
+		return PlayerSettings.player1.controls; */
+	/*#if mobileC
+		var _virtualpad:FlxVirtualPad;
+
+		var trackedinputsUI:Array<FlxActionInput> = [];
+		var trackedinputsNOTES:Array<FlxActionInput> = [];	
+
+		// adding virtualpad to state
+
+		public function addVirtualPad(?DPad:FlxDPadMode, ?Action:FlxActionMode) {
+			_virtualpad = new FlxVirtualPad(DPad, Action);
+			_virtualpad.alpha = 0.75;
+			add(_virtualpad);
+			controls.setVirtualPadUI(_virtualpad, DPad, Action);
+			trackedinputsUI = controls.trackedinputsUI;
+			controls.trackedinputsUI = [];
+		}
+
+		override function destroy() {
+			controls.removeFlxInput(trackedinputsUI);
+			controls.removeFlxInput(trackedinputsNOTES);		
+
+			super.destroy();
+		}
+		#end */
+	override function create()
+	{
+		camBeat = FlxG.camera;
+		var skip:Bool = FlxTransitionableState.skipNextTransOut;
+		super.create();
+
+		// Custom made Trans out
+		if (!skip)
+		{
+			openSubState(new CustomFadeTransition(1, true));
+		}
+		FlxTransitionableState.skipNextTransOut = false;
+	}
+
+	override function update(elapsed:Float)
+	{
+		// everyStep();
+		/*var oldStep:Int = curStep;
+
+			updateCurStep();
+			updateBeat();
+
+			if (oldStep != curStep && curStep > 0)
+				stepHit();
+
+			-- Essa parte aqui serve pro jogo ser compatível com 3 tipos de input diferente
+			-- caso tu mexa o mouse, o mouse fica visivel
+			-- caso tu aperte algo no teclado, o mouse fica invisivel e por aí vai
+			-- Quem sabe eu não tente adaptar isso...
+			if (Main.getMouseVisibility() && (FlxG.keys.pressed.ANY || Utils.GamepadUtil.anyGamepadButtonPressed())){
+				if (!Main.isKeyboard && FlxG.keys.pressed.ANY) Main.isKeyboard = true;
+				else if (Main.isKeyboard && Utils.GamepadUtil.anyGamepadButtonPressed()) Main.isKeyboard = false;
+
+				Main.mouseVisibility(false);
+				
+			}else if (Main.mouse_allowed && #if !mobile FlxG.mouse.justMoved #else Utils.BSLTouchUtils.justTouched() #end){
+				Main.mouseVisibility(true);
+		}*/
+
+		super.update(elapsed);
+	}
+
+	private function updateBeat():Void
+	{
+		// curBeat = Math.floor(curStep / 4);
+	}
+
+	private function updateCurStep():Void
+	{
+		/*
+			var lastChange:BPMChangeEvent = {
+				stepTime: 0,
+				songTime: 0,
+				bpm: 0
+			}
+			for (i in 0...Song.Conductor.bpmChangeMap.length)
+			{
+				if (Song.Conductor.songPosition >= Song.Conductor.bpmChangeMap[i].songTime)
+					lastChange = Song.Conductor.bpmChangeMap[i];
+			}
+
+			curStep = lastChange.stepTime + Math.floor(((Song.Conductor.songPosition - ClientPrefs.noteOffset) - lastChange.songTime) / Song.Conductor.stepCrochet);
+		 */
+	}
+
+	public static function switchState(nextState:FlxState)
+	{
+		// Custom made Trans in
+
+		var curState:Dynamic = FlxG.state;
+		var leState:MusicBeatState = curState;
+		if (!FlxTransitionableState.skipNextTransIn)
+		{
+			leState.openSubState(new CustomFadeTransition(0.7, false));
+			if (nextState == FlxG.state)
+			{
+				CustomFadeTransition.finishCallback = function()
+				{
+					FlxG.resetState();
+				};
+				// trace('resetted');
+			}
+			else
+			{
+				CustomFadeTransition.finishCallback = function()
+				{
+					FlxG.switchState(nextState);
+				};
+				// trace('changed state');
+			}
+			return;
+		}
+		FlxTransitionableState.skipNextTransIn = false;
+		FlxG.switchState(nextState);
+	}
+
+	public static function resetState()
+	{
+		MusicBeatState.switchState(FlxG.state);
+	}
+
+	public static function getState():MusicBeatState
+	{
+		var curState:Dynamic = FlxG.state;
+		var leState:MusicBeatState = curState;
+		return leState;
+	}
+
+	public function stepHit():Void
+	{
+		if (curStep % 4 == 0)
+			beatHit();
+	}
+
+	public function beatHit():Void
+	{
+		// do literally nothing dumbass
+	}
+}
---
+++ src/game/MusicBeatSubstate.hx
@@ -0,0 +1,96 @@
+package game;
+
+import flixel.FlxSubState;
+
+class MusicBeatSubstate extends FlxSubState
+{
+	public function new()
+	{
+		super();
+	}
+
+	private var lastBeat:Float = 0;
+	private var lastStep:Float = 0;
+
+	private var curStep:Int = 0;
+	private var curBeat:Int = 0;
+
+	/*private var controls(get, never):Input.Controls;
+
+		inline function get_controls():Input.Controls
+			return PlayerSettings.player1.controls;
+
+		#if mobileC
+		var _virtualpad:FlxVirtualPad;
+
+		var trackedinputsUI:Array<FlxActionInput> = [];
+		var trackedinputsNOTES:Array<FlxActionInput> = [];	
+
+		// adding virtualpad to state
+		public function addVirtualPad(?DPad:FlxDPadMode, ?Action:FlxActionMode) {
+			_virtualpad = new FlxVirtualPad(DPad, Action);
+			_virtualpad.alpha = 0.75;
+			add(_virtualpad);
+			controls.setVirtualPadUI(_virtualpad, DPad, Action);
+			trackedinputsUI = controls.trackedinputsUI;
+			controls.trackedinputsUI = [];
+		}
+
+		override function destroy() {
+			controls.removeFlxInput(trackedinputsUI);
+			controls.removeFlxInput(trackedinputsNOTES);		
+			
+			super.destroy();
+		}
+		#end */
+	override function update(elapsed:Float)
+	{
+		/*if (Main.getMouseVisibility() && (FlxG.keys.pressed.ANY || Utils.GamepadUtil.anyGamepadButtonPressed())){
+				if (!Main.isKeyboard && FlxG.keys.pressed.ANY) Main.isKeyboard = true;
+				else if (Main.isKeyboard && Utils.GamepadUtil.anyGamepadButtonPressed()) Main.isKeyboard = false;
+
+				Main.mouseVisibility(false);
+				
+			}else if (Main.mouse_allowed && #if !mobile FlxG.mouse.justMoved #else Utils.BSLTouchUtils.justTouched() #end){
+				Main.mouseVisibility(true);
+			}
+
+			//everyStep();
+			var oldStep:Int = curStep;
+
+			updateCurStep();
+			curBeat = Math.floor(curStep / 4);
+
+			if (oldStep != curStep && curStep > 0)
+				stepHit(); */
+
+		super.update(elapsed);
+	}
+
+	private function updateCurStep():Void
+	{
+		/*var lastChange:BPMChangeEvent = {
+				stepTime: 0,
+				songTime: 0,
+				bpm: 0
+			}
+			for (i in 0...Song.Conductor.bpmChangeMap.length)
+			{
+				if (Song.Conductor.songPosition > Song.Conductor.bpmChangeMap[i].songTime)
+					lastChange = Song.Conductor.bpmChangeMap[i];
+			}
+
+			curStep = lastChange.stepTime + Math.floor((Song.Conductor.songPosition - lastChange.songTime) / Song.Conductor.stepCrochet); */
+	}
+
+	public function stepHit():Void
+	{
+		/*if (curStep % 4 == 0)
+			beatHit(); */
+	}
+
+	public function beatHit():Void
+	{
+		// do literally nothing dumbass
+	}
+}
